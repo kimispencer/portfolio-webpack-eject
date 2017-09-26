6f73fa4197fb221d0f19620c89d330eba40ea75c
kimispencer <kimispencer@gmail.com>
2017-09-26 16:48:54 -0400

    console.log cleanup

--- src/components/ImageLoader/ImageLoader.js
+++ src/components/ImageLoader/ImageLoader.js
@@ -27,7 +27,6 @@ class ImageLoader extends React.Component {
 
 	render() {
 		let img = (this.props.src.indexOf('http') > -1) ? this.props.src : require(`../../assets/${this.props.imgKey}/${this.props.src}`);
-console.log(`../../assets/${this.props.imgKey}/${this.props.src}`)
 
 		if(this.props.isBg === false) {
 			return (
